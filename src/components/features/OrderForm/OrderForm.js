7c8254dc90b88675cb3d17b072275153feab50aa
Oskyyy <oskarwro@gmail.com>
2020-01-19 22:43:47 +0100

    Add order options

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,9 +5,19 @@ import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = props => (
   <Row>
+    {pricing.map((option, setOrderOption) => (
+      <Col md={4} key={option.id}>
+        <OrderOption {...option}
+          currentValue={props.options[option.id]}
+          setOrderOption={setOrderOption}
+        />
+      </Col>
+    ))}
     <Col xs={12}>
       <OrderSummary tripCost={props.tripCost} options={props.options} />
     </Col>
@@ -17,6 +27,7 @@ const OrderForm = props => (
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
+  setOrderOption: PropTypes.func,
 };
 
 export default OrderForm;
---
+++ src/components/features/OrderOption/OrderOptionIcons/OrderOptionIcons.js
@@ -0,0 +1,11 @@
+  
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const OrderOptionIcons = props => <div>{props.name}</div>;
+
+OrderOptionIcons.propTypes = {
+  name: PropTypes.string,
+};
+
+export default OrderOptionIcons;
