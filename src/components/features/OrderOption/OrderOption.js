54dcca8d834cbefbb2073ce27abc05e00af30c1b
Oskyyy <oskarwro@gmail.com>
2020-01-19 23:32:55 +0100

    Add order option - Text

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -4,12 +4,14 @@ import OrderOptionDropdown from './OrderOptionDropdown/OrderOptionDropdown';
 import OrderOptionIcons from './OrderOptionIcons/OrderOptionIcons';
 import OrderOptionNumber from './OrderOptionNumber/OrderOptionNumber';
 import OrderOptionCheckboxes from './OrderOptionCheckboxes/OrderOptionCheckboxes';
+import OrderOptionText from './OrderOptionText/OrderOptionText';
 
 const optionTypes = {
   dropdown: OrderOptionDropdown,
   icons: OrderOptionIcons,
   checkboxes: OrderOptionCheckboxes,
   number: OrderOptionNumber,
+  text: OrderOptionText,
 };
 
 const OrderOption = ({ id, setOrderOption, name, type, ...otherProps }) => {
@@ -20,7 +22,8 @@ const OrderOption = ({ id, setOrderOption, name, type, ...otherProps }) => {
     return (
       <div className={styles.component}>
         <h3 className={styles.title}>{name}</h3>
-        <OptionComponent {...otherProps} 
+        <OptionComponent
+          {...otherProps}
           setOptionValue={value => setOrderOption({ [id]: value })}
         />
       </div>
